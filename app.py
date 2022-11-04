54df83bd4eaa52833856bbdac5f00ca4e9e5ae01
Neha Kallamvalli <66886704+nehakmvlli@users.noreply.github.com>
2022-11-04 19:21:39 -0400

    changes to app.py

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ def welcome():
     if request.method == "POST":
         # the user has filled out the quiz and we want to display proper pathway resources
         # return render_template("propertemplate.html")
+       first_name = request.form.get("1. What level of education does you belong to?")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("2.  How much time are you able to spend pursuing an educational program?")
+       return "Your name is "+first_name + last_name
+    
