691fc288772e8c72e31b0446dde183f209520fa6
sahilg13 <55365655+sahilg13@users.noreply.github.com>
2022-11-04 17:49:26 -0400

    Updated app.py to render index-quiz template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,4 +6,9 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET", "POST"])
 def welcome():
     
-    return render_template("homepage.html")
+    # the user has not filled out the quiz yet
+    if request.method == "GET":
+        return render_template("index-quiz.html")
+    
+    # the user has filled out the quiz and we want to display proper pathway resources
+    # return render_template("index-results.html")
