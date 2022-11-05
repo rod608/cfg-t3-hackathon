932541b8c8298c668eb45e9a35df2bdd89076bec
Windsor Nguyen <mn4560@princeton.edu>
2022-11-05 10:44:44 -0400

    fixed index_quiz_spanish path

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ import pandas as pd
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/quiz", methods=["GET", "POST"])
 def welcome():
-    
+
     # the user has not filled out the quiz yet
     if request.method == "GET":
         return render_template("index-quiz.html")
-    
+
     # the user has filled out the quiz
     if request.method == "POST":
         # the user has filled out the quiz and we want to display proper pathway resources
@@ -26,10 +28,13 @@ def welcome():
         schools = schools[schools["Grade"] == first_answer]
         schools = schools[schools["Time_Available"] == second_answer]
         schools = schools[schools["Finances"] == third_answer]
-        schools["Cost of program"] = schools["Cost of program"].apply(lambda x: "$" + str(x) + ".00")
-        schools["Average Salary Outcome"] = schools["Average Salary Outcome"].apply(lambda x: "$" + str(x) + "0")
-        schools.rename(columns = {"Cost(3 diff ranges, annual)": "Cost", "Opportunity_Type":"Opportunity"}, inplace = True)
-        
+        schools["Cost of program"] = schools["Cost of program"].apply(
+            lambda x: "$" + str(x) + ".00")
+        schools["Average Salary Outcome"] = schools["Average Salary Outcome"].apply(
+            lambda x: "$" + str(x) + "0")
+        schools.rename(columns={"Cost(3 diff ranges, annual)": "Cost",
+                       "Opportunity_Type": "Opportunity"}, inplace=True)
+
         if second_answer == "0-1 Year":
             return render_template("result_one_year.html", schools=schools)
         else:
@@ -40,6 +45,7 @@ def welcome():
 def index():
     return render_template("index.html")
 
+
 @app.route("/news")
 def news():
     return render_template("newsletter.html")
@@ -49,17 +55,21 @@ def news():
 def news2():
     return render_template("newsletter.html")
 
+
 @app.route("/espanol")
 def espanol():
     return render_template("espanol.html")
+
 
 @app.route("/resultados")
 def resultados():
     return render_template("resultados.html")
 
-@app.route("/index-quiz-spanish")
+
+@app.route("/index_quiz_spanish")
 def index_quiz_spanish():
     return render_template("index_quiz_spanish.html")
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000)
