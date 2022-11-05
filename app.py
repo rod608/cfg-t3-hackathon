a8cb665c4362839f60ed7314d9b7ce431b8fc22d
rod608 <rod608ney@gmail.com>
2022-11-04 20:13:08 -0400

    Quiz updated, still needs styling

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route("/", methods=["GET", "POST"])
 def welcome():
-    
-    # the user has not filled out the quiz yet
-    if request.method == "GET":
-        return render_template("index-quiz.html")
-    
-    # the user has filled out the quiz
-    #if request.method == "POST":
-        # the user has filled out the quiz and we want to display proper pathway resources
-        # return render_template("propertemplate.html")
-       first_name = request.form.get("1. What level of education does you belong to?")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("2.  How much time are you able to spend pursuing an educational program?")
-       return "Your name is "+first_name + last_name
-    
+    return render_template("index-quiz.html")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000, debug=True)
